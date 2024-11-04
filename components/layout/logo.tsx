2849ece2bf11ec78b6c29006792e9c5859997a3b
GidyMane <muragegideon2000@gmail.com>
2024-11-05 02:04:03 +0300

    logo

--- components/layout/logo.tsx
+++ components/layout/logo.tsx
@@ -5,7 +5,7 @@ export const Logo: React.FC<HTMLChakraProps<'div'>> = (props) => {
   return (
     <chakra.div {...props}>
       <Image 
-        src="/static/favicons/2.svg" 
+        src="/static/favicons/2.png" 
         alt="Logo" 
         width={150} // Adjust the width as needed
         height={50} // Adjust the height as needed
