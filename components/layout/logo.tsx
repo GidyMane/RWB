4c0b3f80f2b4bced987c789c41023fe2a7a98efc
GidyMane <muragegideon2000@gmail.com>
2024-11-05 22:43:20 +0300

    fix logo

--- components/layout/logo.tsx
+++ components/layout/logo.tsx
@@ -1,14 +1,16 @@
-import { chakra, HTMLChakraProps } from '@chakra-ui/react';
+import { chakra, HTMLChakraProps, useColorMode } from '@chakra-ui/react';
 import Image from 'next/image';
 
 export const Logo: React.FC<HTMLChakraProps<'div'>> = (props) => {
+  const { colorMode } = useColorMode(); // Access the current color mode
+
   return (
     <chakra.div {...props}>
-      <Image 
-        src="/static/favicons/2.png" 
-        alt="Logo" 
-        width={150} 
-        height={50} 
+      <Image
+        src={colorMode === 'dark' ? '/static/favicons/4.png' : '/static/favicons/2.png'}
+        alt="Logo"
+        width={150}
+        height={50}
       />
     </chakra.div>
   );
