eeb6e7becb05324ccb53766b3dee94b08a78194b
GidyMane <muragegideon2000@gmail.com>
2024-11-05 23:42:11 +0300

    orange color

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -304,7 +304,7 @@ const HighlightsSection = () => {
             <Tag
               key={value}
               variant="subtle"
-              colorScheme="purple"
+              colorScheme="orange"
               rounded="full"
               px="3"
             >
