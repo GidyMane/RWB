be6219ffd7a05f6cfce275894bc55ddc379d3e56
GidyMane <muragegideon2000@gmail.com>
2024-11-05 17:17:03 +0300

    links

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -115,7 +115,7 @@ const HeroSection: React.FC = () => {
                 </ButtonLink> */}
                 <ButtonLink
                   size="lg"
-                  href="https://demo.saas-ui.dev"
+                  href="#"
                   variant="outline"
                   rightIcon={
                     <Icon
